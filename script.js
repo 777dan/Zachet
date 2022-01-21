f9499c140010ca4951649883a9c3e03618dca839
777dan <danya.nikitin7@gmail.com>
2022-01-21 17:44:47 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,118 @@
+function Machine() {
+    this.state = "stopped";
+    this.time = 2000;
+    this.timer;
+    this.interval;
+}
+
+Machine.prototype.run = function () {
+    this.state = "started";
+    document.write("Hачинаю работать...");
+    document.write(`Время приготовления - ${this.time} `);
+    this.interval = setInterval(function () { document.write(" | ") }.bind(this), 1000);
+    this.timer = setTimeout(this.onReady.bind(this), this.time);
+    document.write(this.state);
+};
+
+Machine.prototype.onReady = function () {
+    clearInterval(this.interval);
+    clearTimeout(this.timer);
+    this.state = "stopped";
+    document.write(`Готово! ${this.state}`);
+};
+
+Machine.prototype.stop = function () {
+    clearInterval(this.interval);
+    clearTimeout(this.timer);
+    this.state = "stopped";
+    document.write(`Принудительное выключение ${this.state}`);
+}
+
+// let machine = new Machine();
+
+// machine.run();
+// let a = document.getElementById("m1");
+// let b = document.getElementById("m2");
+
+// if (document.getElementById("m1").onclick) {
+
+function CoffeeMachine() {
+    this.drink = "вода";
+    Machine.apply(this);
+}
+
+CoffeeMachine.prototype = Object.create(Machine.prototype);
+CoffeeMachine.prototype.constructor = CoffeeMachine;
+
+document.getElementById("m1").onclick = function () {
+
+    CoffeeMachine.prototype.run = function (drink = "вода") {
+        this.drink = drink;
+        // if (this.drink == "латте") {
+        //     this.time = 5000;
+        // }
+        // if (this.drink == "эспрессо") {
+        //     this.time = 3000;
+        // }
+        // else {
+        //     alert("Такого напитка нет!");
+        //     return;
+        // }
+        switch (this.drink) {
+            case "латте":
+                this.time = 5000;
+                break;
+            case "эспрессо":
+                this.time = 3000;
+                break;
+            default:
+                alert("Такого напитка нет!");
+                stop();
+        }
+        document.write(`приготовление: ${this.drink}`);
+        Machine.prototype.run.apply(this);
+
+    };
+
+    let coffeeMachine = new CoffeeMachine();
+    // let a = coffeeMachine.run;
+    coffeeMachine.run(prompt("Укажите напиток"));
+    // coffeeMachine.stop();
+    // }
+}
+
+// if (document.getElementById("m2").onclick == true) {
+function Multivariate() {
+    Machine.apply(this);
+}
+
+Multivariate.prototype = Object.create(Machine.prototype);
+Multivariate.prototype.constructor = Multivariate;
+
+document.getElementById("m2").onclick = function () {
+
+    Multivariate.prototype.runMultivariate = function (food) {
+        this.food = food;
+        switch (this.food) {
+            case "суп":
+                this.time = 10000;
+                break;
+            case "котлеты":
+                this.time = 7000;
+                break;
+            case "пирог":
+                this.time = 15000;
+                break;
+            default:
+                alert("Такого блюда нет!");
+                stop();
+        }
+        document.write(`приготовление: ${this.food}`);
+        Machine.prototype.run.apply(this);
+    }
+
+    let multivariate = new Multivariate();
+    // let b = multivariate.runMultivariate;
+    multivariate.runMultivariate(prompt("Укажите блюдо"));
+    // }
+}
